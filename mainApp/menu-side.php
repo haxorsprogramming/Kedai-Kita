72f496164aba0a14c25d48afb1e595789df7531c
haxorsprogramming <git@haxorsprogrammingclub.id>
2020-01-26 03:42:18 +0700

    dana

--- mainApp/menu-side.php
+++ mainApp/menu-side.php
@@ -6,20 +6,19 @@
     <a href="#!">KK</a>
   </div>
   <ul class="sidebar-menu">
-
       <li class="menu-header">Menu</li>
       <li><a class="nav-link btnDashboard" href="#!"><i class="fas fa-home"></i> <span>Dashboard</span></a></li>
-    <li><a class="nav-link btnKontrol" href="#!"><i class="fas fa-rss"></i> <span>Kontrol Perangkat</span></a></li>
-      <li><a class="nav-link btnStatistik" href="#!"><i class="fas fa-newspaper"></i> <span>Statistik</span></a></li>
-    <li><a class="nav-link btnSetting" href="#!"><i class="fas fa-wrench"></i> <span>Setting</span></a></li>
-      <li><a class="nav-link" href="log_out.php"><i class="fas fa-sign-out-alt"></i> <span>LogOut</span></a></li>
-      <!-- <li class="nav-item dropdown">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+      <li><a class="nav-link btnKontrol" href="#!"><i class="fas fa-rss"></i> <span>Rekap Kedai</span></a></li>
+      <li class="nav-item dropdown">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Data Master</span></a>
         <ul class="dropdown-menu">
-          <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-          <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-          <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+          <li><a class="nav-link" href="#!">Produk</a></li>
+          <li><a class="nav-link" href="#!">Pemasok Barang</a></li>
+          <li><a class="nav-link" href="#!">Pelanggan</a></li>
         </ul>
-      </li> -->
+      </li>
+      <li><a class="nav-link btnKontrol" href="#!"><i class="fas fa-rss"></i> <span>Laporan</span></a></li>
+      <li><a class="nav-link" href="log_out.php"><i class="fas fa-sign-out-alt"></i> <span>LogOut</span></a></li>
+
 
 </aside>
