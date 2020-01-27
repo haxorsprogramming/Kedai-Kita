a0ee584c8fed9fc756bf570a104b9734310c62f1
haxorsprogramming <haxorsuinsu@gmail.com>
2020-01-27 21:00:33 +0700

    disayidan

--- mainApp/produk/formTambahProduk.php
+++ mainApp/produk/formTambahProduk.php
@@ -25,28 +25,44 @@ $kKategori = $link -> query("SELECT * FROM tbl_kategori;");
 <div class="card-body">
 Tambah produk
 <hr/>
+<div class='row'>
 <div class='col-12 col-md-6 col-lg-6'>
 
      <div class="form-group">
         <label>Kode Produk</label>
-        <input type="text" class="form-control" value='<?=$capKode; ?>' readonly>
+        <input type="text" class="form-control" value='<?=$capKode; ?>' readonly id='txtKodeProduk'>
      </div>
      <div class="form-group">
         <label>Nama Produk</label>
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" id='txtNamaProduk'>
      </div>
      <div class="form-group">
         <label>Kategori</label>
-        <select class='form-control'>
+        <select class='form-control' id='txtKategori'>
     <?php
     while($fKat = $kKategori -> fetch_object()){
         $nama = $fKat -> nama;
+        $id = $fKat -> id;
         ?>
-        <option><?=$nama; ?></option>
+        <option value='<?=$id; ?>'><?=$nama; ?></option>
         <?php
     }
     ?>
         </select>
      </div>
 </div>
+
+<div class='col-12 col-md-6 col-lg-6'>
+<div class="form-group">
+        <label>Deksripsi Produk</label>
+        <input type="text" class="form-control">
+     </div>
 </div>
+</div>
+</div>
+
+<script>
+$(document).ready(function(){
+    $('#txtNamaProduk').focus();
+});
+</script>
