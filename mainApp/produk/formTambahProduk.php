6deaddc4304ab46017d4df57896d71a82549e8b9
haxorsprogramming <haxorsuinsu@gmail.com>
2020-01-27 19:58:48 +0700

    sambil liatin nessie judge

--- mainApp/produk/formTambahProduk.php
+++ mainApp/produk/formTambahProduk.php
@@ -1,3 +1,12 @@
 <div class="card-body">
 Tambah produk
+<hr/>
+<div class='col-12 col-md-6 col-lg-6'>
+
+     <div class="form-group">
+        <label>Default Input Text</label>
+        <input type="text" class="form-control">
+     </div>
+
+</div>
 </div>
