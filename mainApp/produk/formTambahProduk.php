43f080eea5990c88b1bf85a3361e43321d0838f8
haxorsprogramming <haxorsuinsu@gmail.com>
2020-01-27 20:20:38 +0700

    ada mbak nissa

--- mainApp/produk/formTambahProduk.php
+++ mainApp/produk/formTambahProduk.php
@@ -1,12 +1,52 @@
+<?php
+session_start();
+if(!ISSET($_SESSION['username'])){
+    header('location:../');
+    die();
+}else{
+
+}
+$user = $_SESSION['username'];
+include('../../conf/db.php');
+
+//kueri ambil kode akhir produk 
+$lastKodeProduk = $link -> query("SELECT kd_produk FROM tbl_produk ORDER BY id DESC LIMIT 0, 1;");
+$fLastProduk = $lastKodeProduk -> fetch_object();
+$kProduk = $fLastProduk -> kd_produk;
+$exProduk = explode("_",$kProduk);
+$kodeProduk = $exProduk[1] + 1;
+$capKode = "P_".$kodeProduk;
+
+//kueri tampil kategori
+$kKategori = $link -> query("SELECT * FROM tbl_kategori;");
+
+
+?>
 <div class="card-body">
 Tambah produk
 <hr/>
 <div class='col-12 col-md-6 col-lg-6'>
 
      <div class="form-group">
-        <label>Default Input Text</label>
+        <label>Kode Produk</label>
+        <input type="text" class="form-control" value='<?=$capKode; ?>' readonly>
+     </div>
+     <div class="form-group">
+        <label>Nama Produk</label>
         <input type="text" class="form-control">
      </div>
-
+     <div class="form-group">
+        <label>Kategori</label>
+        <select class='form-control'>
+    <?php
+    while($fKat = $kKategori -> fetch_object()){
+        $nama = $fKat -> nama;
+        ?>
+        <option><?=$nama; ?></option>
+        <?php
+    }
+    ?>
+        </select>
+     </div>
 </div>
 </div>
